57d118292fa885131812f7579c45e0535fdc544f
Ssssv11 <56876234+Ssssv11@users.noreply.github.com>
2023-03-31 19:25:33 +0800

    添加生成进入config配置目录命令的按钮

--- K8s-Mapping.js
+++ K8s-Mapping.js
@@ -4,23 +4,15 @@
 // @version      1.0
 // @description  K8s Mapping script for Tampermonkey
 // @author       Ssssv
-// @match        *://devops.ks1.wezhuiyi.com/*
+// @match        http://devops.ks1.wezhuiyi.com/*
 // @grant        GM_setClipboard
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-      text = window.getSelection().toString();
-    }
-    return text;
-  }
 
-  function openPrompt() {
-    var selectedText = getSelectionText();
+  function openPrompt1() {
 
     var namespace = prompt('Please enter the Namespace:', '');
     if (namespace === null) {
@@ -41,13 +33,36 @@
     }
   }
 
-  function createIcon() {
+  function openPrompt2() {
+
+    var namespace = prompt('Please enter the Namespace:', '');
+    if (namespace === null) {
+      return;
+    }
+
+    var kubectlCmd = `cd /data/jenkins/workspace/k8s-envs/${namespace}/opt`
+    var result = prompt('Copy and modify the command:', kubectlCmd);
+
+    if (result !== null) {
+      GM_setClipboard(result);
+    }
+  }
+
+  function createIcon1() {
     var icon = document.createElement('div');
-    icon.setAttribute('style', 'position: fixed; z-index:9999; bottom: 20px; right: 20px; width: 30px; height: 30px; border-radius: 50%; background-color: #292a73; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3); cursor: pointer; display: flex; justify-content: center; align-items: center; font-weight: bold; font-size: 10px; color: #f7a400;');
-    icon.textContent = 'K8s';
-    icon.addEventListener('click', openPrompt);
+    icon.setAttribute('style', 'position: fixed; z-index:9999; bottom: 20px; right: 15px; width: 30px; height: 30px; border-radius: 50%; background-color: #292a73; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3); cursor: pointer; display: flex; justify-content: center; align-items: center; font-weight: bold; font-size: 10px; color: #f7a400;');
+    icon.textContent = 'Port';
+    icon.addEventListener('click', openPrompt1);
+    document.body.appendChild(icon);
+  }
+  function createIcon2() {
+    var icon = document.createElement('div');
+    icon.setAttribute('style', 'position: fixed; z-index:9999; bottom: 55px; right: 15px; width: 30px; height: 30px; border-radius: 50%; background-color: #292a73; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3); cursor: pointer; display: flex; justify-content: center; align-items: center; font-weight: bold; font-size: 10px; color: #f7a400;');
+    icon.textContent = 'Lens';
+    icon.addEventListener('click', openPrompt2);
     document.body.appendChild(icon);
   }
 
-    createIcon();
+    createIcon1();
+    createIcon2();
 })();
